f457d525d6dd72e57d9262a76f3f74f9f10dfa15
kozomdoci <zihye.choi@gmail.com>
2023-10-06 11:28:16 +0900

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+–
+faccountBookProject/src/main/java/com/accountbook/accountBookProject/AccountBookProjectApplication.java,8\b\8bb8b76c596e23919e3bf9cffb5373a74048118a
---
+++ accountBookProject/src/main/java/com/accountbook/accountBookProject/AccountBookProjectApplication.java
@@ -0,0 +1,13 @@
+package com.accountbook.accountBookProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBookProjectApplication.class, args);
+	}
+
+}
